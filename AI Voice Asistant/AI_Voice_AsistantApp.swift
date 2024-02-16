0dbc706806899784b11924a6b2aa3693adc0db97
keremdemir <keremdemir1694@gmail.com>
2024-02-16 23:36:57 +0300

    Initial Commit

---
+++ AI Voice Asistant/AI_Voice_AsistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Voice_AsistantApp.swift
+//  AI Voice Asistant
+//
+//  Created by Kerem Demir on 16.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_Voice_AsistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Voice Asistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Voice Asistant
+//
+//  Created by Kerem Demir on 16.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
